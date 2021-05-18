cf99beafa762193958ab6b2d257642cc527c6e92
Robin Khiv <robinkhiv@gmail.com>
2021-05-17 23:01:03 -0700

    rk:added blackjack exercise

--- src/com/qa/runner/Conditional.java
+++ src/com/qa/runner/Conditional.java
@@ -6,6 +6,49 @@ public class Conditional {
 	//and it if is false it will return the multiplication of the two numbers.
 	public static int getOutput(int i, int j, boolean isTrue)
 	{
-		return isTrue ? i+ j : i*j;
+		return isTrue ? i+j : i*j;
+	}
+	//Recreate the following flowchart as a project. Ensure that your logic and outputs match that of the flowchart.
+	//Create a method that takes a single integer A as a parameter.
+	//First evaluate if A is greater than 2000. If true, print "A", if false, print "1"
+	//Next, Directly under print 1 but not outside of the if statement, make another if statement that evaluates if A is greater that 100. If true, print "2", if false, print "3"
+	public static void printFlowChart(int i)
+	{
+		if (i > 2000)
+		{
+			System.out.println("A");
+			if(i > 6000)
+				System.out.println("C");
+			else 
+			{
+				System.out.println("B");
+				if (i > 4000)
+					System.out.println("D");
+				else
+					System.out.println("E");
+			}
+		}
+		else 
+		{
+			System.out.println("1");
+			if (i > 100)
+			{
+				System.out.println("3");
+				if (i > 600)
+					System.out.println("5");
+				else
+				{
+					System.out.println("4");
+					if (i > 500)
+						System.out.println("6");
+					else
+						System.out.println("7");
+				}
+			}
+			else 
+				System.out.println("2");
+		}
+		
+		
 	}
 }
--- src/com/qa/runner/Runner.java
+++ src/com/qa/runner/Runner.java
@@ -5,11 +5,18 @@ public class Runner {
 	public static void main(String[] args) {
 		int sum = 0;
 		int multi = 0;
+		int p1 = 2;
+		int p2 = 4;
+		int closerTo21 = 0;
+		
+		closerTo21 = Blackjack.play(p1,p2);
+		System.out.println("Blackjack: " + closerTo21);
 		
 		sum = Conditional.getOutput(1, 2, true);
 		multi = Conditional.getOutput(3, 3, false);
 		System.out.println("Sum1: " + sum);
 		System.out.println("Multiplcation: " + multi);
+		
 		sum = Conditional.getOutput(1,1, true);
 		System.out.println("Sum2: " + sum);
 
